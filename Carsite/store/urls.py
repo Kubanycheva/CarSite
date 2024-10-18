cca6f9f1591f498943c98074fdf21a3c473570ed
KasymbecovRahat <kasymbecovrahat@gmail.com>
2024-10-18 21:31:09 +0600

    urls

--- Carsite/store/urls.py
+++ Carsite/store/urls.py
@@ -4,19 +4,19 @@ from .views import *
 urlpatterns = [
 
     path('', ProductListViewSet.as_view({'get': 'list', 'post': 'create', }), name='product_list'),
-    path('<int:pk>/', ProductDetailViewSet.as_view({'get': 'list', 'delete': 'destroy', 'put': 'update'}), name='product_detail'),
+    path('<int:pk>/', ProductDetailViewSet.as_view({'get': 'retrieve', 'delete': 'destroy', 'put': 'update'}), name='product_detail'),
 
 
     path('category', CategoryViewSet.as_view({'get': 'list', 'post': 'create', }), name='category_list'),
-    path('category/<int:pk>/', CategoryViewSet.as_view({'get': 'list', 'delete': 'destroy', 'put': 'update'}), name='category_detail'),
+    path('category/<int:pk>/', CategoryViewSet.as_view({'get': 'retrieve', 'delete': 'destroy', 'put': 'update'}), name='category_detail'),
 
 
     path('photo', ProductPhotosViewSet.as_view({'get': 'list', 'post': 'create', }), name='photo_list'),
-    path('photo/<int:pk>/', ProductPhotosViewSet.as_view({'get': 'list', 'delete': 'destroy', 'put': 'update'}), name='photo_detail'),
+    path('photo/<int:pk>/', ProductPhotosViewSet.as_view({'get': 'retrieve', 'delete': 'destroy', 'put': 'update'}), name='photo_detail'),
 
 
     path('review', ReviewViewSet.as_view({'get': 'list', 'post': 'create', }), name='review_list'),
-    path('review/<int:pk>/', ReviewViewSet.as_view({'get': 'list', 'delete': 'destroy', 'put': 'update'}), name='review_detail'),
+    path('review/<int:pk>/', ReviewViewSet.as_view({'get': 'retrieve', 'delete': 'destroy', 'put': 'update'}), name='review_detail'),
 
 
 
